ed0536813ec39f2f88230d1a029635919cf7cb38
prasoon-p <prasoon@arvension.com>
2022-08-11 19:41:18 +0530

    mailer

--- routes/add.routing.js
+++ routes/add.routing.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const basicRoutes = require('./basic.route');
-const accademic =require('./academic.route');
+const accademic = require('./academic.route');
 const expRoutes = require('./exp.route');
 const skillRoutes = require('./skill.route');
 const jobRoutes = require('./job.route');
@@ -11,12 +11,11 @@ const uploadRoutes = require('./upload.route');
 
 router.use('/accademic', accademic);
 router.use('/basics', basicRoutes);
-
-router.use('/exp',expRoutes);
-router.use('/skill',skillRoutes);
-router.use('/job',jobRoutes)
-router.use('/report',basicRoutes,expRoutes,skillRoutes,jobRoutes)
-router.use('/upload',uploadRoutes)
+router.use('/exp', expRoutes);
+router.use('/skill', skillRoutes);
+router.use('/job', jobRoutes)
+router.use('/report', basicRoutes, expRoutes, skillRoutes, jobRoutes)
+router.use('/upload', uploadRoutes)
 
 //router.use('/uploads',uploadRoutes)
 
