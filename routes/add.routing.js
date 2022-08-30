d05f869494b97e8547008b09d85765280d2ac1fa
prasoon-p <prasoon@arvension.com>
2022-08-30 12:45:55 +0530

    learn

--- routes/add.routing.js
+++ routes/add.routing.js
@@ -13,13 +13,11 @@ const learningRoutes = require('./learning.route');
 const visitorRoutes = require('./visitor.route');
 const trainingRoutes = require('./training.route')
 const vacancyRoute = require('./vacancy.route')
-<<<<<<< HEAD
 const EmpTrainingRoute = require('./emptraining.route')
 const FeedbackRoute = require('./feedback.route')
 const TaskRoute = require('./task.route')
 const TokenRoutes = require('./canteen.route')
-=======
->>>>>>> 00dd83dca7d9425721dfb816489078759923b4ba
+const blacklistRoutes=require('./blackList.route')
 
 router.use('/accademic', accademic);
 router.use('/basics', basicRoutes);
@@ -31,20 +29,15 @@ router.use('/upload', uploadRoutes)
 router.use('/depart', deptRoutes)
 router.use('/addtraining', trainingRoutes)
 router.use('/vacancy', vacancyRoute)
-<<<<<<< HEAD
 router.use('/emptraining',EmpTrainingRoute)
 router.use('/feedback',FeedbackRoute)
 router.use('/task',TaskRoute)
 router.use('/token',TokenRoutes)
-//router.use('/uploads',uploadRoutes)
-=======
 router.use('/request',requestRoutes)
 router.use('/leavepackage',leaveRoutes)
 router.use('/learning',learningRoutes)
 router.use('/visitor',visitorRoutes)
-// router.use('/blacklist')
-
->>>>>>> 00dd83dca7d9425721dfb816489078759923b4ba
+router.use('/blacklist',blacklistRoutes)
 
 
 module.exports = router;
