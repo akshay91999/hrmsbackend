ac17fd69c7591bceae6d699a843bc5caf41a5337
prasoon-p <prasoon@arvension.com>
2022-09-09 15:23:27 +0530

    db update6

--- services/basic.service.js
+++ services/basic.service.js
@@ -1,5 +1,6 @@
 const sequelize = require('sequelize');
 const db = require('../config/database');
+const { QueryTypes } = require('sequelize');
 const Basics = require('../model/basic.model')
 const Address = require('../model/address.model')
 const Parents = require('../model/parents.model')
@@ -41,9 +42,9 @@ async function findById(id, res) {
     const t = await db.transaction();
     try {
 
-        const [person, metadata] = await db.query("SELECT b.id, b.firstname  || ' '|| b.lastname AS name,b.gender,b.dob,b.nationality,u.document,a.*,c.*,p.* FROM public.basics AS b,public.addresses AS a,public.contacts AS c,public.parents AS p,public.uploads AS u WHERE b.id=" + id + " AND b.id=a.basic_id AND b.id=c.basic_id AND b.id=p.basic_id AND u.basic_id=b.id AND u.doc_type='photo'", { transaction: t })
+        const person = await db.query("SELECT b.id, b.firstname  || ' '|| b.lastname AS name,b.gender,b.dob,b.nationality,u.document,a.*,c.*,p.* FROM public.basics AS b,public.addresses AS a,public.contacts AS c,public.parents AS p,public.uploads AS u WHERE b.id=" + id + " AND b.id=a.basic_id AND b.id=c.basic_id AND b.id=p.basic_id AND u.basic_id=b.id AND u.doc_type='photo'", { type: QueryTypes.SELECT }, { transaction: t })
         t.commit
-        return (person);
+        return (person.reduce((obj, item) => ({...obj,[item[1]]: item})))
     }
     catch (error) {
         console.log(error);
@@ -74,7 +75,7 @@ async function findall(req, res) {
         const base =await Basics.findAll({attributes:{exclude:['password']}},{transaction: t })
         // const contact =await Contact.findAll({transaction: t })
         // const job= await Job.findAll({transaction: t })
-        const [person, metadata] = await db.query("SELECT b.id,b.firstname  || ' '|| b.lastname AS name,c.contactnumber,c.email,dp.departmentname,ds.designation, u.document FROM public.basics AS b,public.departments AS dp,public.contacts AS c,public.designations AS ds,public.uploads AS u ,public.jobs AS j WHERE b.id=c.basic_id AND u.basic_id=b.id AND j.basic_id=b.id AND j.dp_id=dp.dp_id AND j.ds_id=ds.ds_id AND u.doc_type='photo'", { transaction: t })
+        const [person, metadata] = await db.query("SELECT b.id,b.firstname  || ' '|| b.lastname AS name,b.gender,c.contactnumber,c.email,dp.departmentname,ds.designation, u.document FROM public.basics AS b,public.departments AS dp,public.contacts AS c,public.designations AS ds,public.uploads AS u ,public.jobs AS j WHERE b.id=c.basic_id AND u.basic_id=b.id AND j.basic_id=b.id AND j.dp_id=dp.dp_id AND j.ds_id=ds.ds_id AND u.doc_type='photo'", { transaction: t })
         t.commit();
         if (!base.deletedat) {
             return { message: "success", person };
