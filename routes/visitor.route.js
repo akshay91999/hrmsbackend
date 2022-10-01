c90aaee67bc6ee963fa5482ec3497b5049063a07
prasoon-p <prasoon@arvension.com>
2022-10-01 18:06:10 +0530

    basics5

--- routes/visitor.route.js
+++ routes/visitor.route.js
@@ -7,7 +7,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, './visitor.photo');
+    cb(null, './visitor');
   },
     filename:(req,file,cb)=>{
         cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
